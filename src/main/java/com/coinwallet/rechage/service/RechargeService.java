754959de4871d89b5a6af7141a7fac69f27c0190
John Laizi <liuhuan@qq.com>
2018-03-15 19:11:24 +0800

    update

--- src/main/java/com/coinwallet/rechage/service/RechargeService.java
+++ src/main/java/com/coinwallet/rechage/service/RechargeService.java
@@ -4,12 +4,18 @@ import com.coinwallet.common.InitConfig;
 import com.coinwallet.common.util.AES;
 import com.coinwallet.common.web3j.api.OWalletAPI;
 import com.coinwallet.common.web3j.bean.WalletInfo;
+import com.coinwallet.common.web3j.utils.CommonUtils;
 import com.coinwallet.rechage.controller.req.CreateWalletReq;
 import com.coinwallet.rechage.dao.UserCoinBalanceMapper;
 import com.coinwallet.rechage.entity.MerchantInfo;
 import com.coinwallet.rechage.entity.UserCoinBalance;
+import com.coinwallet.rechage.rabbit.RabbitRechargeConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.web3j.protocol.core.methods.response.Transaction;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Service
 public class RechargeService {
@@ -21,28 +27,43 @@ public class RechargeService {
     private InitConfig initConfig;
 
     /**
-     * create-wallet
-     * @param createWalletReq userid
-     * @param merchantInfo
+     *
+     * @param transaction
      */
-    public UserCoinBalance initUserCoinWallet(CreateWalletReq createWalletReq, MerchantInfo merchantInfo) {
-        UserCoinBalance userCoinBalance = userCoinBalanceMapper.selectByPrimaryKey(createWalletReq.getUserid());
-        if(userCoinBalance==null){
-            userCoinBalance =  new UserCoinBalance();
-        }else{
-            return  userCoinBalance;
+    public void rechargeRecord(Transaction transaction) {
+        String toAddress = CommonUtils.getContractAddressTo(transaction.getInput());
+        UserWalletInfo userWalletInfo = userWalletInfoMapper.selectWalletInfoByAddress(toAddress);
+        if (userWalletInfo != null) {
+            CoinInfo coinInfo = coinInfoMapper.selectByContravtAddress(transaction.getTo());
+            TranscationOrder transcationOrder = new TranscationOrder();
+            transcationOrder.setCreatedTime(new Date());
+            insertTransacntionOrder(transaction, toAddress, RabbitRechargeConfig.ORDER_TYPE_USER_RECHARGE,coinInfo, RabbitRechargeConfig.ORDER_STATUS_PEEDING, transcationOrder);
+
         }
-        try {
-            WalletInfo walletInfo = OWalletAPI.generateWallet();
-            userCoinBalance.setCoinAddress(walletInfo.getWalletAddress());
-            userCoinBalance.setCoinName(merchantInfo.getMerchantName());
-            userCoinBalance.setMerchantId(merchantInfo.getId());
-            userCoinBalance.setPrivatekey(AES.encrypt(walletInfo.getPrivateKey(),initConfig.deskey));
-            userCoinBalance.setUserid(createWalletReq.getUserid());
-            userCoinBalanceMapper.insertSelective(userCoinBalance);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return userCoinBalance;
+
+
+    }
+
+    /**
+     *
+     * @param transaction
+     * @param toAddress
+     * @param coinInfo
+     * @param orderStatus
+     * @param transcationOrder
+     */
+    public void insertTransacntionOrder(Transaction transaction, String toAddress, Integer transcationType, CoinInfo coinInfo, Integer orderStatus, TranscationOrder transcationOrder) {
+
+        transcationOrder.setTxHash(transaction.getHash());
+        transcationOrder.setTranscationType(transcationType);
+        transcationOrder.setCoinName(coinInfo.getCoinName());
+        transcationOrder.setCoinId(coinInfo.getId());
+        transcationOrder.setCoinNum(CommonUtils.getSTAmount(transaction.getInput()));
+        transcationOrder.setCreatedTime(new Date());
+        transcationOrder.setGasPrice(new BigDecimal(transaction.getGasPrice()));
+        transcationOrder.setFromAddress(transaction.getFrom());
+        transcationOrder.setToAddress(toAddress);
+        transcationOrder.setOrderStatus(orderStatus);
+        transcationOrderMapper.insertSelective(transcationOrder);
     }
 }
